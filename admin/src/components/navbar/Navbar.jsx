ca2831475798c8ccd13a1fd4758d870bb36583f8
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-10-19 20:03:01 +0530

    chart

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./navbar.css";
 import { Link, useNavigate } from "react-router-dom";
+import { CloseBtn, MenuBar } from "../../assets/svg/Icon";
 // import { AppContext } from "../../context/AppContext";
 
 const Navbar = ({ navLinks, type }) => {
@@ -21,37 +22,26 @@ const Navbar = ({ navLinks, type }) => {
       {/* Mobile menu */}
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="exampleModal"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog m-0">
-          <div class="modal-content">
-            <div class="modal-header">
+        <div className="modal-dialog m-0">
+          <div className="modal-content">
+            <div className="modal-header">
               <h2 className="mb-0 menuHeading">Menu</h2>
               <button
                 type="button"
-                class="btnClose"
+                className="btnClose"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                >
-                  <path
-                    d="M5.91614 5.0078L9.81 1.11387C10.0634 0.86058 10.0634 0.451037 9.81 0.197779C9.55672 -0.0555096 9.14717 -0.0555096 8.89391 0.197779L4.99995 4.09168L1.10615 0.197779C0.852737 -0.0555096 0.443346 -0.0555096 0.190057 0.197779C-0.0633525 0.451067 -0.0633525 0.86058 0.190057 1.11387L4.08383 5.0078L0.190088 8.9017C-0.0633221 9.15498 -0.0633221 9.56453 0.190088 9.81778C0.250169 9.87804 0.321564 9.92582 0.40017 9.9584C0.478776 9.99097 0.563044 10.0077 0.648133 10.0076C0.813955 10.0076 0.979837 9.94413 1.10618 9.81778L4.99995 5.92389L8.89391 9.81778C8.954 9.87803 9.0254 9.92581 9.104 9.95838C9.18261 9.99095 9.26687 10.0077 9.35196 10.0076C9.51778 10.0076 9.68366 9.94413 9.81 9.81778C10.0634 9.5645 10.0634 9.15498 9.81 8.9017L5.91614 5.0078Z"
-                    fill="#BE2AED"
-                  />
-                </svg>
+                <CloseBtn />
               </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <ul className="navbar-nav d-flex">
                 {navLinks.map((item, i) => (
                   <NavLink
@@ -77,31 +67,7 @@ const Navbar = ({ navLinks, type }) => {
             data-bs-toggle="modal"
             data-bs-target="#exampleModal"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              fill="none"
-            >
-              <g clip-path="url(#clip0_193_51224)">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M0 2.58696C0 2.26279 0.272259 2 0.608108 2H13.3919C13.7277 2 14 2.26279 14 2.58696C14 2.91112 13.7277 3.17391 13.3919 3.17391H0.608108C0.272259 3.17391 0 2.91112 0 2.58696ZM0 6.5C0 6.17583 0.272259 5.91304 0.608108 5.91304H8.09459C8.43044 5.91304 8.7027 6.17583 8.7027 6.5C8.7027 6.82417 8.43044 7.08696 8.09459 7.08696H0.608108C0.272259 7.08696 0 6.82417 0 6.5ZM0 10.413C0 10.0889 0.272259 9.82609 0.608108 9.82609H10.527C10.8629 9.82609 11.1351 10.0889 11.1351 10.413C11.1351 10.7372 10.8629 11 10.527 11H0.608108C0.272259 11 0 10.7372 0 10.413Z"
-                  fill="#464255"
-                  stroke="#464255"
-                  stroke-width="0.2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_193_51224">
-                  <rect width="14" height="14" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
+            <MenuBar />
           </button>
 
           <div className="d-flex align-items-center gap-5">
--- admin/src/screens/Dashboard/Dashboard.jsx
+++ admin/src/screens/Dashboard/Dashboard.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../layout/Layout";
+import { Increase } from "../../assets/svg/Icon";
+import "./dashboard.css";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
 
 const Dashboard = () => {
   const Data = [
@@ -25,15 +45,161 @@ const Dashboard = () => {
     },
   ];
 
+  const monthlyIncome = [
+    { name: "Jan", inr: 4000 },
+    { name: "Feb", inr: 3000 },
+    { name: "Mar", inr: 2000 },
+    { name: "Apr", inr: 2780 },
+    { name: "May", inr: 1890 },
+    { name: "Jun", inr: 2390 },
+    { name: "Jul", inr: 3490 },
+  ];
+
+  const donatChart = [
+    { name: "Category A", value: 400 },
+    { name: "Category B", value: 300 },
+    { name: "Category C", value: 200 },
+    { name: "Category D", value: 100 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  const [lineCharWidth, setLineChartWidth] = useState(640);
+  const [lineCharHeight, setLineChartHeight] = useState(300);
+
+  const [pieCharWidth, setPieChartWidth] = useState(640);
+  const [pieCharHeight, setPieChartHeight] = useState(240);
+
+  useEffect(() => {
+    const updateChartWidth = () => {
+      if (window.innerWidth < 576) {
+        setLineChartWidth(window.innerWidth - 40);
+        setPieChartWidth(window.innerWidth - 40);
+        setPieChartHeight(265);
+        setLineChartHeight(250);
+      } else if (window.innerWidth < 768) {
+        setLineChartWidth(500);
+        setPieChartWidth(500);
+        setPieChartHeight(265);
+        setLineChartHeight(320);
+      } else if (window.innerWidth < 992) {
+        setLineChartWidth(370);
+        setPieChartWidth(430);
+        setPieChartHeight(265);
+        setLineChartHeight(320);
+      } else if (window.innerWidth < 1200) {
+        setLineChartWidth(460);
+        setPieChartWidth(460);
+        setPieChartHeight(250);
+        setLineChartHeight(320);
+      } else if (window.innerWidth < 1400) {
+        setLineChartWidth(550);
+        setPieChartWidth(550);
+        setPieChartHeight(250);
+        setLineChartHeight(320);
+      }
+    };
+
+    updateChartWidth();
+    window.addEventListener("resize", updateChartWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateChartWidth);
+    };
+  }, []);
+
   return (
     <Layout>
       <div className="restaurant-dash">
-        <div class="container" style={{ paddingTop: "6rem" }}>
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <div className="container" style={{ paddingTop: "6rem" }}>
+          <div className="d-sm-flex align-items-center justify-content-between mb-4">
+            <h3 className="mb-0 text-gray-800">Dashboard</h3>
           </div>
-          <div class="row">
-            
+
+          <div className="dashboard_cards_lg d-md-flex d-none">
+            {Data.map(({ title, amount, rate }, i) => (
+              <div key={i} className="dashboard_card mobile">
+                <div>
+                  <span>{title}</span>
+                </div>
+                <div className="dashboard_card_bottom">
+                  <h5>₹{amount}</h5>
+                  <div className="dashboard_card_bottom_right">
+                    <span>{rate}</span>
+                    <div>
+                      <Increase />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="dashboard_cards_sm d-block d-md-none">
+            <Swiper
+              className="Cards"
+              slidesPerView={1}
+              spaceBetween={30}
+              autoplay={{ delay: 100 }}
+            >
+              {Data.map(({ title, amount, rate }, i) => (
+                <SwiperSlide key={i} className="dashboard_card mobile">
+                  <div>
+                    <span>{title}</span>
+                  </div>
+                  <div className="dashboard_card_bottom">
+                    <h5>₹{amount}</h5>
+                    <div className="dashboard_card_bottom_right">
+                      <span>{rate}</span>
+                      <div>
+                        <Increase />
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          <div className="mt-5 row">
+            <div className="col-md-6 col-12">
+              <LineChart
+                width={lineCharWidth}
+                height={lineCharHeight}
+                data={monthlyIncome}
+              >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid stroke="#ccc" />
+                <Line type="monotone" dataKey="inr" stroke="#8884d8" />
+                <Tooltip label="name" formatter={(value) => [`INR ${value}`]} />
+                <Legend />
+              </LineChart>
+            </div>
+
+            <div className="col-md-6 col-12 mt-5 mt-md-0 d-flex align-items-center">
+              <PieChart width={pieCharWidth} height={pieCharHeight}>
+                <Pie
+                  data={donatChart}
+                  dataKey="value"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#8884d8"
+                  label
+                >
+                  {donatChart.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+              </PieChart>
+            </div>
           </div>
         </div>
       </div>
