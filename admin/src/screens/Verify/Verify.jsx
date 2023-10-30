3a0891815a43c836b9c12f1aab262c1ca3c649e4
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-10-30 17:42:56 +0530

    seacrh

--- admin/src/screens/Verify/Verify.jsx
+++ admin/src/screens/Verify/Verify.jsx
@@ -12,6 +12,7 @@ import {
 import Protected from "../../routes/Protected";
 import { useQuery } from "@tanstack/react-query";
 import { dbObject } from "../../helper/api";
+import LoadingSecond from "../../components/laoding/LoadingSecond";
 
 const Verify = () => {
   const data = [
@@ -48,10 +49,13 @@ const Verify = () => {
   // });
 
   const [total, setTotal] = useState(0);
+  const limit = 2;
 
   const fetchRestaurants = async () => {
     const { data } = await dbObject.get(
-      `/all/pending-restaurants?page=${page + 1}&limit=${2}&search=${search}`
+      `/all/pending-restaurants?page=${
+        page + 1
+      }&limit=${limit}&search=${search}`
     );
 
     setTotal(data?.total || 0);
@@ -69,10 +73,6 @@ const Verify = () => {
     queryFn: fetchRestaurants,
     staleTime: 5 * 60 * 1000,
   });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   if (error) {
     return <div>Error fetching data {error.message}</div>;
@@ -98,6 +98,8 @@ const Verify = () => {
                       name="search"
                       id="search"
                       placeholder="Search"
+                      value={search}
+                      onChange={(e) => setSearch(e.target.value)}
                     />
                   </div>
                   <button
@@ -145,69 +147,71 @@ const Verify = () => {
                   <th>Price</th>
                   <th className=" text-center">Status</th>
                 </thead>
-                <tbody className="tbl">
-                  {restaurnats?.map((obj, i) => (
-                    <tr key={i} className="list_card">
-                      <td className="align-middle" style={{ minWidth: 100 }}>
-                        <p className="fw-bold mb-1 ">{obj.name}</p>
-                      </td>
-                      <td
-                        className=" align-middle"
-                        style={{ minWidth: 100, maxWidth: 100 }}
-                      ></td>
-                      <td
-                        className=" align-middle text-capitalize"
-                        style={{ minWidth: 100 }}
-                      >
-                        {obj.category}
-                      </td>
-                      <td
-                        className=" align-middle text-capitalize"
-                        style={{ minWidth: 100 }}
-                      >
-                        {obj.type}
-                      </td>
-                      <td className=" align-middle" style={{ minWidth: 100 }}>
-                        ₹{obj.price}
-                      </td>
-                      <td className=" align-middle" style={{ width: "20%" }}>
-                        <div className=" d-flex gap-2 justify-content-evenly align-items-center m-0 p-0">
-                          <button
-                            className="dashboard_container_status_btn"
-                            style={{
-                              background: "#E88B00",
-                              cursor: "default",
-                            }}
-                          >
-                            <Edit />
-                            <span>Deactive</span>
-                          </button>
-                          <button
-                            className="dashboard_container_status_btn"
-                            style={{
-                              background: "#DC3545",
-                              cursor: "default",
-                            }}
-                            onClick={() => handleDelete(obj._id)}
-                          >
-                            <Delete />
-                            <span>Delete</span>
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
+                {!isLoading && (
+                  <tbody className="tbl">
+                    {restaurnats?.map((obj, i) => (
+                      <tr key={i} className="list_card">
+                        <td className="align-middle" style={{ minWidth: 100 }}>
+                          <p className="fw-bold mb-1 ">{obj.name}</p>
+                        </td>
+                        <td
+                          className=" align-middle"
+                          style={{ minWidth: 100, maxWidth: 100 }}
+                        ></td>
+                        <td
+                          className=" align-middle text-capitalize"
+                          style={{ minWidth: 100 }}
+                        >
+                          {obj.category}
+                        </td>
+                        <td
+                          className=" align-middle text-capitalize"
+                          style={{ minWidth: 100 }}
+                        >
+                          {obj.type}
+                        </td>
+                        <td className=" align-middle" style={{ minWidth: 100 }}>
+                          ₹{obj.price}
+                        </td>
+                        <td className=" align-middle" style={{ width: "20%" }}>
+                          <div className=" d-flex gap-2 justify-content-evenly align-items-center m-0 p-0">
+                            <button
+                              className="dashboard_container_status_btn"
+                              style={{
+                                background: "#E88B00",
+                                cursor: "default",
+                              }}
+                            >
+                              <Edit />
+                              <span>Deactive</span>
+                            </button>
+                            <button
+                              className="dashboard_container_status_btn"
+                              style={{
+                                background: "#DC3545",
+                                cursor: "default",
+                              }}
+                              onClick={() => handleDelete(obj._id)}
+                            >
+                              <Delete />
+                              <span>Delete</span>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
 
-                  <div className="mt-2 p-0 bg-danger "></div>
-                  <tr className="list_card">
-                    <td colSpan={7}>
-                      <div className="  my-2 d-flex  justify-content-end align-items-center gap-1 ">
-                        <span onClick={() => setPage(Math.max(page - 1, 0))}>
-                          prev
-                        </span>
+                    <div className="mt-2 p-0 bg-danger "></div>
+                    <tr className="list_card">
+                      <td colSpan={7}>
+                        <div className="  my-2 d-flex  justify-content-end align-items-center gap-1 ">
+                          <span onClick={() => setPage(Math.max(page - 1, 0))}>
+                            prev
+                          </span>
 
-                        {Array.from({ length: Math.round(total / 2) }).map(
-                          (_, i) => (
+                          {Array.from({
+                            length: Math.round(total / limit),
+                          }).map((_, i) => (
                             <div
                               key={i}
                               className="border border-white text-white d-inline-block"
@@ -222,23 +226,35 @@ const Verify = () => {
                             >
                               {i + 1}
                             </div>
-                          )
-                        )}
+                          ))}
 
-                        <span
-                          onClick={() =>
-                            setPage(
-                              Math.min(page + 1, Math.round(total / 2) - 1)
-                            )
-                          }
-                        >
-                          Next
-                        </span>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
+                          <span
+                            onClick={() =>
+                              setPage(
+                                Math.min(
+                                  page + 1,
+                                  Math.round(total / limit) - 1
+                                )
+                              )
+                            }
+                          >
+                            Next
+                          </span>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                )}
               </table>
+
+              {isLoading && (
+                <div
+                  className="d-flex align-items-center justify-content-center w-100"
+                  style={{ height: 100 }}
+                >
+                  <LoadingSecond />
+                </div>
+              )}
             </div>
           </div>
         </div>
